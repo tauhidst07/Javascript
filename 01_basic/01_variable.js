501a37c5b69c0006edd92dd2f081147b76d030a3
Md Tauhid Ansari <116505896+tauhidst07@users.noreply.github.com>
2024-06-02 08:21:55 +0000

    varibale declaration

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId =12123 
+let accountName= "tauhid" 
+var accountPassword="12345" //should not use 
+accountCity="Darbhanga" // not recommended 
+
+// accountId= 34566 not possible 
+ 
+accountName = "ansari" 
+accountPassword="54321"
+
+
+console.table([accountId,accountName,accountPassword,accountCity])
