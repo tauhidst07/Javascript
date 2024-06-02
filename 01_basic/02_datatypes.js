f8bf4bc02ef26449585e1f7987ee6e40c4b3abe8
Md Tauhid Ansari <116505896+tauhidst07@users.noreply.github.com>
2024-06-02 09:26:37 +0000

    data types in javascript

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version 
+
+let name = "tauhid" 
+let age = 22 
+let isActive = true 
+
+/*   Data Types */ 
+
+// number 
+// bigint 
+// String 
+// boolean => true/false 
+// null => standalone value 
+// undefined => 
+// symbol => used for uniqueness 
+
+// object  
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); //object
