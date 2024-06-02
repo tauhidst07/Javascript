98161a4f9ce6deefb4e7e7ecde20f2441916c08e
Md Tauhid Ansari <116505896+tauhidst07@users.noreply.github.com>
2024-06-02 16:57:12 +0000

    basic conversion operations

---
+++ 01_basic/03_conversionOpeartions.js
@@ -0,0 +1,36 @@
+let score = "33"
+
+// console.log(typeof score); 
+// console.log(typeof(score));  
+
+const vlaueInNumber = Number(score); 
+
+// console.log(typeof vlaueInNumber); 
+// console.log(vlaueInNumber);  
+
+/* Number conversion of following Values */
+
+// "33" => 33 
+// "33abc"=>NaN 
+// true => 1 false => 0 
+// null => 0  
+// undefined => NaN   
+
+ 
+/* Boolean Conversion */ 
+
+const isLoggedIn = undefined
+
+const valueInBoolean = Boolean(isLoggedIn); 
+
+console.log(valueInBoolean);  
+
+// 1 => true 
+// 0 =>false 
+// "" => false 
+// "hello" => true 
+// null => false 
+// undefined => false
+
+
+
