aa704afa23a2465d2b31c8f6008336896f433f2a
Md Tauhid Ansari <116505896+tauhidst07@users.noreply.github.com>
2024-06-03 12:17:02 +0530

    Update 03_conversionOpeartions.js

--- 01_basic/03_conversionOpeartions.js
+++ 01_basic/03_conversionOpeartions.js
@@ -33,4 +33,18 @@ console.log(valueInBoolean);
 // undefined => false
 
 
+// **************************Operations*************************** 
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2");  //32 
+
+// console.log(+true)  // 1 
+
+// console.log(+"")   // 0
+
+console.log(typeof(+"3")) // type => number
+
+
 
