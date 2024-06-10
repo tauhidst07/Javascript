1da2a54b3620d0e36571d5f8379117cd4739e7ca
Md Tauhid Ansari <116505896+tauhidst07@users.noreply.github.com>
2024-06-10 20:45:44 +0530

    Create 01_array.js

---
+++ 02_basic/01_array.js
@@ -0,0 +1,39 @@
+const arr =[0,1,2,3,4,5] 
+// console.log( arr) 
+
+// const arr2 = new Array(1,2,3,4,5) 
+// console.log( typeof arr2) 
+
+// console.log(arr[2]) //^ accessing array values 
+
+// console.log(arr.length) //^ finding array length 
+
+//! Array Methods 
+
+// arr.push(10) //^ push element at end of array 
+// console.log(arr) 
+// arr.pop() //^ remove element from end 
+
+// arr.unshift(10) //^ push element at first index 
+// console.log(arr) 
+
+// arr.shift(); 
+// console.log(arr) //^ remove element from start 
+
+// console.log(arr.includes(10)) //^ checks if a value is present or not 
+// console.log(arr.indexOf(4)) //^ returns index of an element in an array 
+
+// const newArr = arr.join();  //^ combines array element into a single string (takes seperator as an argument)
+// console.log(newArr)   
+
+// console.log("A ",arr);  
+
+// const myn1 =arr.slice(1,3) //^ returns a slice of array from given range to less than upper range(upper range is excluded)
+                           //^ does not change the original array
+// console.log(myn1); 
+
+// console.log("B ",arr) 
+
+// const myn2 = arr.splice(1,3); //^ remove a slice of array from original array and returns it including upper range 
+// console.log(myn2);
+// console.log("C ",arr)
